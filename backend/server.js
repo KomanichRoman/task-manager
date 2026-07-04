7475a3a194785cc276f2cafb4e81f9d56752f367
Komanich Roman <gorotro_215@mail.ru>
2026-07-04 20:52:39 +0300

    feat: проверка GET и POST для задач

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Сервер запущен на порту ${PORT}`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
